6bf06a52965e644809dd7ec53bb23b0958dd6c03
jaehyeok0914 <103923213+jaehyeok0914@users.noreply.github.com>
2024-12-02 10:40:33 +0900

    Create 10869.c

---
+++ Task_Folder/10869.c
@@ -0,0 +1,15 @@
+// 10869 사칙연산
+#include <stdio.h>
+
+int main (void){
+    int a, b;
+    scanf("%d %d", &a, &b);
+    printf("%d\n", a+b);
+    printf("%d\n", a-b);
+    printf("%d\n", a*b);
+    printf("%d\n", a/b);
+    printf("%d\n", a%b);
+    
+    return 0;
+}
+// 제출번호 : 87076733
