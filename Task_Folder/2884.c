37aa22cdc5310914727a9f18d6e9bf033541d3bb
jaehyeok0914 <103923213+jaehyeok0914@users.noreply.github.com>
2024-12-02 10:50:34 +0900

    Create 2884.c

---
+++ Task_Folder/2884.c
@@ -0,0 +1,18 @@
+// 2884 알람시계
+#include <stdio.h>
+
+int main(void){
+    int h, m;
+    scanf("%d %d", &h, &m);
+    if (m>=45)
+        printf("%d %d", h, m-45);
+    else
+        if (h == 0)
+            printf("%d %d", h+23, m+15);
+        else
+            printf("%d %d", h-1, m+15);
+    
+    return 0;
+}
+
+// 제출번호 : 87076991
