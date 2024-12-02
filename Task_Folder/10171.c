f1d08c788cc56b8e58ac0d63b698fbb5fd9a2922
jaehyeok0914 <103923213+jaehyeok0914@users.noreply.github.com>
2024-12-02 10:25:30 +0900

    Create 10171.c

---
+++ Task_Folder/10171.c
@@ -0,0 +1,11 @@
+// 10171 고양이
+#include <stdio.h>
+
+int main(void){
+    printf("\\    /\\\n");
+    printf(" )  ( \')\n");
+    printf("(  /  )\n");
+    printf(" \\(__)|");
+}
+
+// 제출 번호 : 87076353
