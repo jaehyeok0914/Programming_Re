4ec321d378d7fd182d03843e105475b4092427b6
jaehyeok0914 <103923213+jaehyeok0914@users.noreply.github.com>
2024-12-03 10:07:45 +0900

    Create 2753.c

---
+++ Task_Folder/2753.c
@@ -0,0 +1,15 @@
+// 2753 윤년
+#include <stdio.h>
+
+int main(void){
+    int year;
+    scanf("%d", &year);
+    if (year%4==0 && year%100!=0 || year%400==0)
+        printf("1");
+    else
+        printf("0");
+    
+    return 0;
+}
+
+제출번호 : 87111650
