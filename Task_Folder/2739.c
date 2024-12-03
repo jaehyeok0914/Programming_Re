1e3e76dde976de063e2cb90a900d556316507612
jaehyeok0914 <103923213+jaehyeok0914@users.noreply.github.com>
2024-12-03 10:11:38 +0900

    Create 2739.c

---
+++ Task_Folder/2739.c
@@ -0,0 +1,14 @@
+// 2739 구구단
+#include <stdio.h>
+
+int main(void){
+    int a, b;
+    scanf("%d", &b);
+    for (a=1; a<10; a++){
+        printf("%d * %d = %d\n", b, a, b*a);
+    }
+    
+    return 0;
+}
+
+제출 번호 : 87111759
